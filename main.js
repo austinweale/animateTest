f9e535d47c0cdffcc430715eb19e1d54f10cb9ab
studentuser <studentuser@studentusers-MacBook-Pro.local>
2015-12-06 14:35:18 -0800

    cleaned up code

--- main.js
+++ main.js
@@ -2,18 +2,12 @@ var myApp = angular.module('myApp', ['ngAnimate']);
 
 myApp.controller('mainController', function($scope){
     $scope.list = [];
-    $scope.count = 0;
 
     $scope.add = function() {
       for (var i = 0; i < 6; i++) {
         $scope.list.push("item");
-        if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
-            $scope.$apply();
-        }
-       
       } 
 
-     }
-
+   }
   
 })
