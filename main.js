98421256d9e32a1611166b9c7ee8f5d343d73d71
studentuser <studentuser@studentusers-MacBook-Pro.local>
2015-12-06 14:33:06 -0800

    working for example with 6 boxes

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
 var myApp = angular.module('myApp', ['ngAnimate']);
 
 myApp.controller('mainController', function($scope){
-    $scope.data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $scope.list = [];
+    $scope.count = 0;
 
+    $scope.add = function() {
+      for (var i = 0; i < 6; i++) {
+        $scope.list.push("item");
+        if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
+            $scope.$apply();
+        }
+       
+      } 
 
-    
+     }
+
+  
 })
-.animation('.slide', [function() {
-  return {
-    // make note that other events (like addClass/removeClass)
-    // have different function input parameters
-    enter: function(element, doneFn) {
-      $(element).fadeIn(1000, doneFn);
-
-      // remember to call doneFn so that angular
-      // knows that the animation has concluded
-    },
-
-    move: function(element, doneFn) {
-      $(element).fadeIn(1000, doneFn);
-    },
-
-    leave: function(element, doneFn) {
-      $(element).fadeOut(1000, doneFn);
-    }
-  }
-}]);
